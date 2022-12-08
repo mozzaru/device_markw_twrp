a56cc02e2eb5bcc67716d142c982e3be96bf0d2c
GhostMaster69-dev <rathore6375@gmail.com>
2022-12-08 19:08:09 +0530

    vince: Stop using `$(PRODUCT_RELEASE_NAME)` Now
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- twrp_vince.mk
+++ twrp_vince.mk
@@ -4,14 +4,12 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-PRODUCT_RELEASE_NAME := vince
-
 # Inherit from vince device
 $(call inherit-product, device/xiaomi/vince/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := $(PRODUCT_RELEASE_NAME)
-PRODUCT_NAME := twrp_$(PRODUCT_RELEASE_NAME)
+PRODUCT_DEVICE := vince
+PRODUCT_NAME := twrp_vince
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 5 Plus
